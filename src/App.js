76f2b8e25c4994430856a1916ef2ced3f0ad1788
Adissy <guilhermeadissy@gmail.com>
2019-10-17 15:23:42 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Home from './pages/home/home';
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/consultant">
+        </Route>
+        <Route path="/smartphone">
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
