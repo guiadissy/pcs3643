0e50338bb52b4e79f26373316fcca195629cc64e
Florian Schopp <11448723@labsoft.local>
2019-10-17 17:47:51 -0300

    Consultor

---
+++ src/pages/Consultor/consultor.strings.js
@@ -0,0 +1,15 @@
+export const companyName = 'QualComp';
+
+export const companySlogan = 'A maior plataforma de consultoria do Brasil';
+
+export const enterButtonText = 'Entrar';
+
+export const consultorButtonText = 'Seja um consultor';
+
+export const title = `Seja um Consultor!`;
+
+export const info = 'Tem conhecimento sobre algum tipo de produto e quer ganhar dinheiro com isso?';
+
+export const email = 'E-Mail';
+
+export const celular = 'Celular';
