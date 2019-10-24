cf28b1e46dce5d7e0e9c4998922bd9252cf2eb64
unknown <Beogival Wagner Lucas Santos Junior>
2019-10-24 14:53:06 -0300

    Beo FIxes

--- src/pages/home/home.styled.js
+++ src/pages/home/home.styled.js
@@ -9,11 +9,15 @@ export const Header = styled.div`
 
 export const AlignLeft = styled.div`
   display: flex;
+  margin-left: 18px;
+  margin-block: auto;
   align-items: center;
 `;
 
 export const AlignRight = styled.div`
   display: flex;
+  margin-right: 18px;
+  margin-block: auto;
 `;
 
 export const Background = styled.div`
