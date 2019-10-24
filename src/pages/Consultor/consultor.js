2c992b41232a2be318907ea250be7b053de53f4f
Adissy <guilhermeadissy@gmail.com>
2019-10-24 15:51:21 -0300

    header-fix

--- src/pages/Consultor/consultor.js
+++ src/pages/Consultor/consultor.js
@@ -3,61 +3,61 @@ import { makeStyles } from '@material-ui/core/styles';
 import {Button, Typography, Divider, TextField} from '@material-ui/core'
 import { Header, AlignLeft, AlignRight, Background, FullWidthDivider, PageWrapper, Body, radioBox, RadioBox } from './consultor.styled';
 import { companyName, companySlogan, consultorButtonText, enterButtonText, subtitle, categoria, title, info, email,celular }
-    from './consultor.strings';
+  from './consultor.strings';
 import {history} from "../../components/Routes/Routes";
 import {consultantUrl, homeUrl} from "../../components/Routes/Routes.strings";
 import {TypographyWithMargin} from "../home/home.styled";
 
 const useStyles = makeStyles(theme => ({
-    textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-    },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
 }));
 
 const Consultor = () => {
 
-    const goToHomeRoute = () => {
-        history.push(homeUrl);
-    };
+  const goToHomeRoute = () => {
+    history.push(homeUrl);
+  };
 
 
-    return (
-        <PageWrapper>
-            <Header>
-                <AlignLeft onClick={goToHomeRoute}>
-                    <Typography variant="h4">{companyName}</Typography>
-                    <TypographyWithMargin variant="h6">{companySlogan}</TypographyWithMargin>
-                </AlignLeft>
-                <AlignRight>
-                    <Button>{enterButtonText}</Button>
-                    <Button variant="contained" color="primary">{consultorButtonText}</Button>
-                </AlignRight>
-            </Header>
-            <FullWidthDivider />
-            
-            <Body>
-                <Typography variant="h1" >{title}</Typography>
-                <p><Typography>{info}</Typography></p>
-                <div>
-                    <TextField
-                        margin="normal"
-                        variant="outlined"
-                        label={email}
-                        type="email"
-                        name="email"
-                        autoComplete="email"/>
-                </div>
-                <div>
-                    <TextField variant="outlined"
-                        label={celular}
-                        margin="normal"
-                    />
-                </div>
-                <Button variant="contained" color="primary">{consultorButtonText}</Button>
-            </Body>
-        </PageWrapper>
-    );
+  return (
+    <PageWrapper>
+      <Header>
+        <AlignLeft onClick={goToHomeRoute}>
+          <Typography variant="h4">{companyName}</Typography>
+          <TypographyWithMargin variant="h6">{companySlogan}</TypographyWithMargin>
+        </AlignLeft>
+        <AlignRight>
+          <Button>{enterButtonText}</Button>
+          <Button variant="contained" color="primary">{consultorButtonText}</Button>
+        </AlignRight>
+      </Header>
+      <FullWidthDivider />
+
+      <Body>
+        <Typography variant="h1" >{title}</Typography>
+        <p><Typography>{info}</Typography></p>
+        <div>
+          <TextField
+            margin="normal"
+            variant="outlined"
+            label={email}
+            type="email"
+            name="email"
+            autoComplete="email"/>
+        </div>
+        <div>
+          <TextField variant="outlined"
+                     label={celular}
+                     margin="normal"
+          />
+        </div>
+        <Button variant="contained" color="primary">{consultorButtonText}</Button>
+      </Body>
+    </PageWrapper>
+  );
 };
 
 export default Consultor;
